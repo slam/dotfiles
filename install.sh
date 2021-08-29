de148743c7354e5b2a2139a51609053e9382501d
Sahn Lam <slam@users.noreply.github.com>
2021-08-29 02:13:37 +0000

    Add neovim and zsh

--- install.sh
+++ install.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y curl git
+sudo apt-get install -y \
+    curl \
+    fasd \
+    git
 
 sudo chsh -s /usr/bin/zsh $USER
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
 if [ ! -d "$HOME/.asdf" ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 fi
+
+. $HOME/.asdf/asdf.sh
+asdf plugin add neovim
+asdf install neovim stable
+asdf global neovim stable
+
+echo "Symlinking all files and directories starting with '.' into $HOME"
+
+for dotfile in "$PWD"/.*; do
+    # Skip `..` and '.'
+    [[ $dotfile =~ \.{1,2}$ ]] && continue
+    # Skip Git related
+    [[ $dotfile =~ \.git$ ]] && continue
+    [[ $dotfile =~ \.gitignore$ ]] && continue
+    [[ $dotfile =~ \.gitattributes$ ]] && continue
+
+    echo "Symlinking $dotfile"
+    ln -sf "$dotfile" "$HOME"
+done
