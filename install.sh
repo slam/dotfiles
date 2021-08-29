360bbf3dcf07adfa09f468f690555bebec0f60b0
Sahn Lam <slam@users.noreply.github.com>
2021-08-29 04:28:51 +0000

    Add p10k

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+fi
+
 if [ ! -d "$HOME/.asdf" ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 fi
