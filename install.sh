dd741de1e88d761ac4f858530221df8b37ee9b80
Sahn Lam <slam@users.noreply.github.com>
2021-08-29 06:41:27 +0000

    Use stow

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 sudo apt-get update
 sudo apt-get install -y \
     curl \
     fasd \
-    git
+    git \
+    stow
 
 sudo chsh -s /usr/bin/zsh $USER
 
@@ -12,7 +15,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"]; then
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 fi
 
@@ -25,16 +28,5 @@ asdf plugin add neovim
 asdf install neovim stable
 asdf global neovim stable
 
-echo "Symlinking all files and directories starting with '.' into $HOME"
-
-for dotfile in "$PWD"/.*; do
-    # Skip `..` and '.'
-    [[ $dotfile =~ \.{1,2}$ ]] && continue
-    # Skip Git related
-    [[ $dotfile =~ \.git$ ]] && continue
-    [[ $dotfile =~ \.gitignore$ ]] && continue
-    [[ $dotfile =~ \.gitattributes$ ]] && continue
-
-    echo "Symlinking $dotfile"
-    ln -sf "$dotfile" "$HOME"
-done
+stow git
+stow zsh
