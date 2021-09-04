d17e88f15c8a9f09db6e087bdf13686ed9a28e81
Sahn Lam <slam@users.noreply.github.com>
2021-09-03 21:51:16 -0700

    Add skeleton setup for mac os

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # dotfiles
+
+My dotfiles. It uses GNU stow to manage symlinks.
+
+## Mac OS
+
+It assumes the following have already been installed by hand:
+
+- Homebrew
+- 1Password7
+- Google Chrome
+- .ssh configs and keys
+
+## Ubuntu 18.04
+
+Primarily for my remote dev machine at work, which I access via SSH only.
--- install.sh
+++ install.sh
@@ -2,14 +2,56 @@
 
 set -euo pipefail
 
-sudo apt-get update
-sudo apt-get install -y \
-    curl \
-    fasd \
-    git \
-    stow
+uname=$(uname)
 
-sudo chsh -s /usr/bin/zsh $USER
+case $uname in
+"Darwin")
+    brew update
+    brew install \
+        fasd \
+        git \
+        neovim \
+        stow
+
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+    ;;
+"Linux")
+    sudo apt-get update
+    sudo apt-get install -y \
+        curl \
+        fasd \
+        git \
+        stow
+
+    sudo chsh -s /usr/bin/zsh $USER
+
+    if [ ! -d "$HOME/.asdf" ]; then
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+    fi
+
+    . $HOME/.asdf/asdf.sh
+    asdf plugin add neovim || true
+    asdf install neovim stable
+    asdf global neovim stable
+
+    if [ -n "$CODER_USERNAME" ]; then
+        if [ ! -d "$HOME/.neovim2" ]; then
+            pip install --user virtualenv
+            "$HOME/.local/bin/virtualenv" "$HOME/.neovim2"
+            "$HOME/.neovim2/bin/pip" install neovim
+        fi
+
+        if [ ! -d "$HOME/.neovim3" ]; then
+            python3.7 -m venv "$HOME/.neovim3"
+            "$HOME/.neovim3/bin/pip" install neovim
+        fi
+
+        mkdir -p "$HOME/.npm-packages"
+        npm config set prefix "$HOME/.npm-packages"
+    fi
+
+    ;;
+esac
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -17,31 +59,6 @@ fi
 
 if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
-fi
-
-if [ ! -d "$HOME/.asdf" ]; then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
-fi
-
-. $HOME/.asdf/asdf.sh
-asdf plugin add neovim || true
-asdf install neovim stable
-asdf global neovim stable
-
-if [ -n "$CODER_USERNAME" ]; then
-    if [ ! -d "$HOME/.neovim2" ]; then
-        pip install --user virtualenv
-        "$HOME/.local/bin/virtualenv" "$HOME/.neovim2"
-        "$HOME/.neovim2/bin/pip" install neovim
-    fi
-
-    if [ ! -d "$HOME/.neovim3" ]; then
-        python3.7 -m venv "$HOME/.neovim3"
-        "$HOME/.neovim3/bin/pip" install neovim
-    fi
-
-    mkdir -p "$HOME/.npm-packages"
-    npm config set prefix "$HOME/.npm-packages"
 fi
 
 stow git
